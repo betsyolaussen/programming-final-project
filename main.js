7715e5f75f4de3cf1415eb4b5c2afb7a794f14c8
Betsy Olaussen <betsyolaussen@icloud.com>
2019-05-09 15:50:53 -0500

    Home page and bath bombs page complete

--- main.js
+++ main.js
@@ -15,15 +15,15 @@ function closeNav() {
 // Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("myImg");
+var img = $('.myImg');
+var modalImg = $("#modal-image");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+img.click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -31,15 +31,5 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
-
-var i;
-var elements = document.getElementsByClassName("row");
-
-function four() {
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "25%";  // IE10
-    elements[i].style.flex = "25%";
-  }
 }
 
